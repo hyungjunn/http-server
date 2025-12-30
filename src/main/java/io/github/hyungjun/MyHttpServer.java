cebb56c4a6b75d13782802cd9d54539c7c08f0f0
hyungjunn <hotfirebulldog@gmail.com>
2025-12-30 13:23:38 +0900

    Add HTTP server with socket connection

---
+++ src/main/java/io/github/hyungjun/MyHttpServer.java
@@ -0,0 +1,52 @@
+package io.github.hyungjun;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class MyHttpServer {
+    private final int port;
+    private final MyHttpHandler handler;
+
+    public MyHttpServer(int port, MyHttpHandler handler) {
+        this.port = port;
+        this.handler = handler;
+    }
+
+    public void start() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server started on port " + port);
+            while (true) {
+                Socket client = serverSocket.accept();
+                handleClient(client);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleClient(Socket client) {
+        try (client;
+             BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+             OutputStream os = client.getOutputStream()
+        ) {
+            String requestLine = br.readLine();
+            MyHttpRequest request = MyHttpRequest.from(requestLine);
+            MyHttpResponse response = handler.handle(request);
+            os.write(response.toText().getBytes(UTF_8));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        FileReader fileReader = new FileSystemReader("src/main/resources/static");
+        MyHttpHandler handler = new MyHttpHandler(fileReader);
+        MyHttpServer server = new MyHttpServer(8080, handler);
+        server.start();
+    }
+}
